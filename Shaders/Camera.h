9cacfdb831c22ff762591c7145376d2d0de41c0c
seanlego23 <seanlego23@gmail.com>
2022-06-19 19:48:58 -0400

    Add comments and field of view float

--- Shaders/Camera.h
+++ Shaders/Camera.h
@@ -2,8 +2,16 @@
 
 #include <glm/glm.hpp>
 
+/*
+* Camera object
+*/
 struct Camera {
+	//Location of camera (World Coordinates)
 	glm::vec3 loc;
+
+	//Location camera is looking at (World Coordinates)
 	glm::vec3 lookAt;
-	glm::vec3 fov;
+
+	//Horizontal Field of View (1.0f = 90 degrees)
+	float fov;
 };
