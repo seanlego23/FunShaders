da4323089c83849ddabb9b4ccb897d1cec38930f
seanlego23 <seanlego23@gmail.com>
2022-06-19 19:53:37 -0400

    Remove camera variables

--- Shaders/shader_inputs.h
+++ Shaders/shader_inputs.h
@@ -4,11 +4,8 @@
 
 struct shader_inputs {
 	glm::vec2 resolution;
-	glm::vec2 translation;
-	glm::vec2 scale;
-	glm::vec2 offset;
 	glm::vec2 cursorPos;
 	float elapsedTime;
 	float zoom;
-	float zoomSum;
+	float zoomRaw;
 };
