d92123baf2e9f0b355c97d18a60eb3962e7e8f74
seanlego23 <seanlego23@gmail.com>
2022-06-22 10:42:33 -0400

    Add send_programs function

--- Shaders/shader_inputs.h
+++ Shaders/shader_inputs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<glad/glad.h>
 #include<glm/glm.hpp>
 
 struct shader_inputs {
@@ -8,4 +9,12 @@ struct shader_inputs {
 	float elapsedTime;
 	float zoom;
 	float zoomRaw;
+
+	void send_uniforms(GLuint program) const {
+		glUniform2f(glGetUniformLocation(program, "resolution"), resolution.x, resolution.y);
+		glUniform2f(glGetUniformLocation(program, "cursorPos"), cursorPos.x, cursorPos.y);
+		glUniform1f(glGetUniformLocation(program, "elapsedTime"), elapsedTime);
+		glUniform1f(glGetUniformLocation(program, "zoom"), zoom);
+		glUniform1f(glGetUniformLocation(program, "zoomRaw"), zoomRaw);
+	}
 };
