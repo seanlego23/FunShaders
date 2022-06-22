4e1fc244d796174c3f9aaf871e07494f4068869c
seanlego23 <seanlego23@gmail.com>
2022-06-22 10:44:04 -0400

    Define mandelbrot shader_object

--- Shaders/mandelbrot.cpp
+++ Shaders/mandelbrot.cpp
@@ -1,1 +1,9 @@
 #include "mandelbrot.h"
+
+mandelbrot::mandelbrot() : shader_object("data/mandelbrot.glsl") {
+
+}
+
+void mandelbrot::setup_render() {
+
+}
--- Shaders/mandelbrot.h
+++ Shaders/mandelbrot.h
@@ -3,6 +3,12 @@
 #include "shader_object.h"
 
 class mandelbrot : public shader_object {
+public:
+
+	mandelbrot();
+	~mandelbrot() override = default;
+
+	void setup_render() override;
 
 };
 
