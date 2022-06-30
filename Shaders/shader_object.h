0ac908e6dcabae4b1af625833434f3340436914f
seanlego23 <seanlego23@gmail.com>
2022-06-29 21:07:21 -0400

    Remove shader class as a base class

--- Shaders/shader_object.h
+++ Shaders/shader_object.h
@@ -3,21 +3,32 @@
 #include "shader.h"
 #include "shader_inputs.h"
 
-class shader_object : public shader {
+class shader_object {
+
+	shader _mainShader;
+	shader_inputs _inputs;
+
 protected:
 
-	shader_object() = delete;
 	explicit shader_object(const char* shader_file);
 
 public:
 
-	shader_inputs inputs;
+	shader_object() = delete;
 
-	~shader_object() override = default;
+	virtual ~shader_object() = default;
+
+	shader_inputs* get_inputs();
 
 	virtual void mouse_button(int button, int action, int mods);
 
 	virtual void key_input(int key, int scancode, int action, int mods);
+
+	virtual bool has_input_shaders() const = 0;
+
+	virtual int input_shaders_count() const = 0;
+
+	virtual bool setup_input_shader(const int index) = 0;
 
 };
 
